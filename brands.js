ce950292e600d80bea1594c3e427713ed8fc669c
Lynch <lynliac@dunwoody.edu>
2019-02-13 14:21:27 -0600

    Inital commit of index01.html and brands.js files

---
+++ brands.js
@@ -0,0 +1,194 @@
+var data = [
+  {
+    "clothingBrand": "Nike",
+    "yearFounded": 1964,
+    "headquartersLocation": "Beaverton, OR",
+    "netWorth": "34.7 billion",
+    "stockPrice": 82.36,
+    "numOfEmployees": "74,400"
+  },
+  {
+    "clothingBrand": "Adidas",
+    "yearFounded": 1949,
+    "headquartersLocation": "Herzogenaurach, Germany",
+    "netWorth": "19 billion",
+    "stockPrice": 226,
+    "numOfEmployees": "56,888"
+  },
+  {
+    "clothingBrand": "The North Face",
+    "yearFounded": 1968,
+    "headquartersLocation": "Alameda, CA",
+    "netWorth": "33.38 billion",
+    "stockPrice": 85.78,
+    "numOfEmployees": "2,638"
+  },
+  {
+    "clothingBrand": "Patagonia",
+    "yearFounded": 1973,
+    "headquartersLocation": "Ventura, CA",
+    "netWorth": "1.5 billion",
+    "stockPrice": "Private",
+    "numOfEmployees": "1,000"
+  },
+  {
+    "clothingBrand": "Uniqlo",
+    "yearFounded": 1949,
+    "headquartersLocation": "Yamaguchi, Japan",
+    "netWorth": "16.4 billion",
+    "stockPrice": 448.89,
+    "numOfEmployees": "30,000"
+  },
+  {
+    "clothingBrand": "Banana Republic",
+    "yearFounded": 1978,
+    "headquartersLocation": "San Francisco, CA",
+    "netWorth": "Unknown",
+    "stockPrice": 24.9,
+    "numOfEmployees": "150,000"
+  },
+  {
+    "clothingBrand": "Lululemon",
+    "yearFounded": 1998,
+    "headquartersLocation": "Vancouver, Canada",
+    "netWorth": "3.6 billion",
+    "stockPrice": 148.62,
+    "numOfEmployees": "13,400"
+  },
+  {
+    "clothingBrand": "Vans",
+    "yearFounded": 1996,
+    "headquartersLocation": "Anaheim, CA",
+    "netWorth": "3 billion",
+    "stockPrice": 23.15,
+    "numOfEmployees": "4,000"
+  },
+  {
+    "clothingBrand": "Pacsun",
+    "yearFounded": 1980,
+    "headquartersLocation": "Anaheim, CA",
+    "netWorth": "797.79 million",
+    "stockPrice": 0.01,
+    "numOfEmployees": "10,300"
+  },
+  {
+    "clothingBrand": "Urban Outfitters",
+    "yearFounded": 1970,
+    "headquartersLocation": "Philadelphia, PA",
+    "netWorth": "3.45 billion",
+    "stockPrice": 30.9,
+    "numOfEmployees": "16,330"
+  },
+  {
+    "clothingBrand": "Zara",
+    "yearFounded": 1974,
+    "headquartersLocation": "A Coruna, Spain",
+    "netWorth": "25.34 billion",
+    "stockPrice": 24.13,
+    "numOfEmployees": "10,000"
+  },
+  {
+    "clothingBrand": "Champion",
+    "yearFounded": 1919,
+    "headquartersLocation": "Rochester, NY",
+    "netWorth": "643 million",
+    "stockPrice": 18.81,
+    "numOfEmployees": "68,000"
+  },
+  {
+    "clothingBrand": "H&M",
+    "yearFounded": 1947,
+    "headquartersLocation": "Stockholm, Sweden",
+    "netWorth": "25.19 billion",
+    "stockPrice": 3.02,
+    "numOfEmployees": "148,000"
+  },
+  {
+    "clothingBrand": "Converse",
+    "yearFounded": 1908,
+    "headquartersLocation": "Malden, MA",
+    "netWorth": "1.886 billion",
+    "stockPrice": 82.36,
+    "numOfEmployees": "2,658"
+  },
+  {
+    "clothingBrand": "Vineyard Vines",
+    "yearFounded": 1998,
+    "headquartersLocation": "Martha's Vineyard, MA",
+    "netWorth": "1 billion",
+    "stockPrice": "Private",
+    "numOfEmployees": "3,000"
+  },
+  {
+    "clothingBrand": "Columbia",
+    "yearFounded": 1938,
+    "headquartersLocation": "Portland, OR",
+    "netWorth": "2.46 billion",
+    "stockPrice": 104.74,
+    "numOfEmployees": "7,000"
+  },
+  {
+    "clothingBrand": "Under Armour",
+    "yearFounded": 1996,
+    "headquartersLocation": "Washington, D.C",
+    "netWorth": "5 billion",
+    "stockPrice": 21.98,
+    "numOfEmployees": "15,800"
+  },
+  {
+    "clothingBrand": "Carhartt",
+    "yearFounded": 1889,
+    "headquartersLocation": "Dearborn, MI",
+    "netWorth": "406.3 million",
+    "stockPrice": "None",
+    "numOfEmployees": "4,000"
+  },
+  {
+    "clothingBrand": "Gucci",
+    "yearFounded": 1921,
+    "headquartersLocation": "Florence, Italy",
+    "netWorth": "7.1 billion",
+    "stockPrice": 140,
+    "numOfEmployees": "1,000"
+  },
+  {
+    "clothingBrand": "Dior",
+    "yearFounded": 1946,
+    "headquartersLocation": "Paris, France",
+    "netWorth": "4.687 billion",
+    "stockPrice": 379,
+    "numOfEmployees": "84,981"
+  },
+  {
+    "clothingBrand": "Armani",
+    "yearFounded": 1975,
+    "headquartersLocation": "Milan, Italy",
+    "netWorth": "2.98 billion",
+    "stockPrice": "None",
+    "numOfEmployees": "2,011"
+  },
+  {
+    "clothingBrand": "Dolce & Gabanna",
+    "yearFounded": 1985,
+    "headquartersLocation": "Milan, Italy",
+    "netWorth": "13.1 billion",
+    "stockPrice": "None",
+    "numOfEmployees": "3,150"
+  },
+  {
+    "clothingBrand": "Cartier",
+    "yearFounded": 1847,
+    "headquartersLocation": "Paris, France",
+    "netWorth": "6.1 billion",
+    "stockPrice": "None",
+    "numOfEmployees": 669
+  },
+  {
+    "clothingBrand": "Louis Vuitton",
+    "yearFounded": 1854,
+    "headquartersLocation": "Paris, France",
+    "netWorth": "9.9 billion",
+    "stockPrice": 66.49,
+    "numOfEmployees": "121,289"
+  }
+]
